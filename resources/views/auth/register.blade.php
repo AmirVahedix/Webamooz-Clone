b4fc5f7a86c41df815f537433d90094574b07453
amirvahedix <amirvahedix@gmail.com>
2021-10-08 17:06:39 +0330

    Added ValidMobile and ValidPassword rule

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,7 @@
             <x-input name="password" type="password" class="txt-l" placeholder="رمز عبور" autocomplete="new-password" required />
             <x-input name="password_confirmation" class="txt-l" type="password" placeholder="تکرار رمز عبور" autocomplete="new-password" required />
 
-            <span class="rules">رمز عبور باید حداقل ۶ کاراکتر و ترکیبی از حروف بزرگ، حروف کوچک، اعداد و کاراکترهای غیر الفبا مانند !@#$%^&*() باشد.</span>
+            <span class="rules">رمز عبور باید حداقل 8 کاراکتر و ترکیبی از حروف بزرگ، حروف کوچک، اعداد و کاراکترهای غیر الفبا مانند !@#$%^&*() باشد.</span>
             <br>
             <button class="btn continue-btn">ثبت نام و ادامه</button>
         </div>
