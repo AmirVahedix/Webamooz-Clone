a4a627227fafd5f33fe87379a95a342e9f28bb29
amirvahedix <amirvahedix@gmail.com>
2021-10-12 20:54:37 +0330

    Created course table migration

--- modules/AmirVahedix/Course/Database/Migrations/2021_10_12_171216_creat_courses_table.php
+++ modules/AmirVahedix/Course/Database/Migrations/2021_10_12_171216_creat_courses_table.php
@@ -13,7 +13,26 @@ class CreatCoursesTable extends Migration
      */
     public function up()
     {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('teacher_id');
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->string('title');
+            $table->string('slug');
+            $table->float('priority')->nullable();
+            $table->string('price');
+            $table->string('percent');
+            $table->enum('type', ['free', 'paid']);
+            $table->enum('status', ['completed', 'pending', 'locked']);
+            $table->text('description');
+            $table->timestamps();
 
+            $table->foreign('category_id')->references('id')->on('categories')
+                ->onDelete('set null')->onUpdate('cascade');
+
+            $table->foreign('teacher_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
     }
 
     /**
