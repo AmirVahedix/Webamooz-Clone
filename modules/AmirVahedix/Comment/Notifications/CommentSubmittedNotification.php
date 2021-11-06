e444c9021a1b61cf4f639d8b06497c5a08ca080e
amirvahedix <amirvahedix@gmail.com>
2021-11-06 11:49:49 +0330

    Added telegram channel and send telegram notification

--- modules/AmirVahedix/Comment/Notifications/CommentSubmittedNotification.php
+++ modules/AmirVahedix/Comment/Notifications/CommentSubmittedNotification.php
@@ -8,6 +8,8 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use NotificationChannels\Telegram\TelegramChannel;
+use NotificationChannels\Telegram\TelegramMessage;
 
 class CommentSubmittedNotification extends Notification
 {
@@ -22,13 +24,22 @@ class CommentSubmittedNotification extends Notification
 
     public function via($notifiable): array
     {
-        return ['mail'];
+        return ['mail', TelegramChannel::class];
     }
 
     public function toMail($notifiable): CommentSubmittedMail
     {
         return (new CommentSubmittedMail($this->comment))
             ->to($notifiable->email);
+    }
+
+    public function toTelegram()
+    {
+        return TelegramMessage::create()
+            ->to(970171405)
+            ->content("یک دیدگاه جدید برای دوره شما در وب آموز ارسال شده است.")
+            ->button('مشاهده دوره', route('courses.single', $this->comment->commentable->slug))
+            ->button('مدیریت کامنت‌ها', route('dashboard.comments.index'));
     }
 
     public function toArray($notifiable): array
